2e636103c93302da504c8a93480f5e272383e1f8
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2024-10-10 08:46:35 +0700

    update normal bots do not appear/disappear near the player

--- Assets/Scripts/Bot Spawner/BotSpawner.cs
+++ Assets/Scripts/Bot Spawner/BotSpawner.cs
@@ -5,16 +5,35 @@ using UnityEngine;
 public class BotSpawner : MonoBehaviour
 {
     [SerializeField] private Waypoint[] waypoints;
+    [SerializeField] private float startTime = 5;
     [SerializeField] private float spawnRate = 3;
+
+    private Transform player;
+    private Transform[] botRacers;
 
     private void Start()
     {
-        InvokeRepeating(nameof(SpawnBot), 2, spawnRate);
+        FindRacers();
+        InvokeRepeating(nameof(SpawnBot), startTime, spawnRate);
+    }
+
+    private void FindRacers()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        GameObject[] botRacerObjs = GameObject.FindGameObjectsWithTag("Racer");
+        botRacers = new Transform[botRacerObjs.Length];
+        for (int i = 0; i < botRacers.Length; i++)
+        {
+            botRacers[i] = botRacerObjs[i].transform;
+        }
     }
 
     private void SpawnBot()
     {
-
+        if (!IsSpawnValid())
+        {
+            return;
+        }
         var bot = VehiclesPool.Instance.GetPooledObject();
         if (bot != null )
         {
@@ -25,5 +44,27 @@ public class BotSpawner : MonoBehaviour
             bot.transform.position = transform.position;
             bot.transform.LookAt(waypoints[index].transform);
         }
+    }
+
+    private bool IsSpawnValid()
+    {
+        if (player == null || botRacers == null)
+        {
+            return true;
+        }
+        float playerDistance = 250;
+        float botRacerDistance = -20;
+        if (Vector3.Distance(transform.position, player.position) < playerDistance)
+        {
+            return false;
+        }
+        foreach (Transform botRacer in botRacers)
+        {
+            if (Vector3.Distance(transform.position, botRacer.position) < botRacerDistance)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- Assets/Scripts/Bot Spawner/NormalBot.cs
+++ Assets/Scripts/Bot Spawner/NormalBot.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class NormalBot : MonoBehaviour
 {
     [SerializeField] private float duration = 10;
-
+    private Transform player;
+    private float playerDistance = 500;
     private float timer = 0;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
     private void Update()
     {
         timer += Time.deltaTime;
-        if (timer > duration)
+        if (timer > duration && Vector3.Distance(transform.position, player.position) > playerDistance)
         {
             timer = 0;
             gameObject.SetActive(false);
