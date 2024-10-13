8613836bca293d6c0f018c3781326fdb04d69504
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2024-10-13 16:26:01 +0700

    update track3.5.6

---
+++ Assets/Scripts/Arrowmovement.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class ArrowMovement : MonoBehaviour
+{
+    public float speed = 20f; 
+    void Update()
+    {
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Destroyer"))
+        {
+            Destroy(gameObject); 
+        }
+    }
+}
---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ArrowSpawner : MonoBehaviour
+{
+    public GameObject arrowPrefab;
+    public float spawnInterval = 2f; 
+    public Vector3 arrowRotation; 
+
+    void Start()
+    {
+        InvokeRepeating("SpawnArrow", 0f, spawnInterval);
+    }
+
+    void SpawnArrow()
+    {
+        Quaternion rotation = Quaternion.Euler(arrowRotation);
+        Instantiate(arrowPrefab, transform.position, rotation);
+    }
+}
