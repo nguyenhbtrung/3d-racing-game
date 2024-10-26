28ea5f069d26e23e799d7f82d060a12f50a4eb3c
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2024-10-26 07:56:26 +0700

    add panomaric minimap

--- Assets/Scripts/UI/InGameUIHandler.cs
+++ Assets/Scripts/UI/InGameUIHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,10 @@ using UnityEngine.SceneManagement;
 public class InGameUIHandler : MonoBehaviour
 {
     [SerializeField] private GameObject optionsPanel;
+    [SerializeField] private Camera minimapCamera;
+    [SerializeField] private Camera panomaricCamera;
+    [SerializeField] private GameObject panomaricMinimap;
+    [SerializeField] private Transform panomaricDestination;
 
     private void Update()
     {
@@ -13,6 +18,78 @@ public class InGameUIHandler : MonoBehaviour
         {
             Pause();
         }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            SwitchMinimap();
+        }
+    }
+
+    private void SwitchMinimap()
+    {
+        if (!panomaricMinimap.activeInHierarchy)
+        {
+            panomaricCamera.transform.SetPositionAndRotation(minimapCamera.transform.position, minimapCamera.transform.rotation);
+            panomaricCamera.orthographicSize = minimapCamera.orthographicSize;
+            StartCoroutine(MinimapToPanomaric());
+        }
+        else
+        {
+            panomaricCamera.transform.SetPositionAndRotation(minimapCamera.transform.position, minimapCamera.transform.rotation);
+            panomaricCamera.orthographicSize = 1020.0f;
+            
+            StartCoroutine(MinimapFromPanomaric());
+        }
+    }
+
+    private IEnumerator MinimapToPanomaric()
+    {
+        float duration = 0.5f;
+        float timeElapsed = 0.0f;
+        Vector3 startPos = minimapCamera.transform.position;
+        Vector3 endPos = panomaricDestination.position;
+        Quaternion startRot = minimapCamera.transform.rotation;
+        Quaternion endRot = panomaricDestination.rotation;
+        float startCamSize = minimapCamera.orthographicSize * 2f;
+        float endCamSize = 1020.0f;
+        panomaricMinimap.SetActive(true);
+        while (timeElapsed <= duration)
+        {
+            float k = timeElapsed / duration;
+            k = (k > 1) ? 1 : k;
+            panomaricCamera.transform.SetPositionAndRotation(Vector3.Lerp(startPos, endPos, k), Quaternion.Lerp(startRot, endRot, k));
+            panomaricCamera.orthographicSize = Mathf.Lerp(startCamSize, endCamSize, k);
+
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+        panomaricCamera.transform.SetPositionAndRotation(Vector3.Lerp(startPos, endPos, 1), Quaternion.Lerp(startRot, endRot, 1));
+        panomaricCamera.orthographicSize = Mathf.Lerp(startCamSize, endCamSize, 1);
+
+    }
+
+    private IEnumerator MinimapFromPanomaric()
+    {
+        float duration = 0.5f;
+        float timeElapsed = 0.0f;
+        Vector3 startPos = panomaricDestination.position;
+        Vector3 endPos = minimapCamera.transform.position;
+        Quaternion startRot = panomaricDestination.rotation;
+        Quaternion endRot = minimapCamera.transform.rotation;
+        float startCamSize = 1020.0f;
+        float endCamSize = minimapCamera.orthographicSize * 2f;
+        while (timeElapsed <= duration)
+        {
+            float k = timeElapsed / duration;
+            k = (k > 1) ? 1 : k;
+            panomaricCamera.transform.SetPositionAndRotation(Vector3.Lerp(startPos, endPos, k), Quaternion.Lerp(startRot, endRot, k));
+            panomaricCamera.orthographicSize = Mathf.Lerp(startCamSize, endCamSize, k);
+
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+        panomaricCamera.transform.SetPositionAndRotation(Vector3.Lerp(startPos, endPos, 1), Quaternion.Lerp(startRot, endRot, 1));
+        panomaricCamera.orthographicSize = Mathf.Lerp(startCamSize, endCamSize, 1);
+        panomaricMinimap.SetActive(false);
     }
 
     public void BackToMenuScene()
